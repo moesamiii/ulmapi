373dd33a1a247bb73adf322d3c538381c8e75992
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2025-06-17 16:37:05 +0300

    updated

--- src/components/Checkout/SavedAddresses.jsx
+++ src/components/Checkout/SavedAddresses.jsx
@@ -145,9 +145,6 @@ const SavedAddresses = () => {
             >
               <div className="flex items-start justify-between">
                 <div className="flex items-center gap-2">
-                  <p className="font-bold text-sm text-[#1C1C1C]">
-                    {address.givenName} {address.surName}
-                  </p>
                   {i === 0 ? (
                     <div className="w-5 h-5 rounded-full bg-[#0099FF] flex items-center justify-center">
                       <svg
@@ -168,6 +165,9 @@ const SavedAddresses = () => {
                   ) : (
                     <div className="w-5 h-5 rounded-full border border-gray-400" />
                   )}
+                  <p className="font-bold text-sm text-[#1C1C1C]">
+                    {address.givenName} {address.surName}
+                  </p>
                 </div>
                 <div className="flex gap-2">
                   <button
